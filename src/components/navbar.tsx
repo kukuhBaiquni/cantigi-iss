c8e0659c86ce1f88a084adf17e01fec7f9be3701
kukuhBaiquni <kukuh.baiquni@gmail.com>
2022-06-28 22:55:19 +0700

    setting

---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full bg-pink-300 h-20">
+      test
+    </nav>
+  );
+}
---
+++ src/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, {
+  Html, Head, Main, NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en" role="document">
+        <Head>
+          <link href="/favicon.ico" rel="icon" />
+          <link href="https://fonts.gstatic.com" rel="preconnect" />
+          <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Nunito&display=swap&family=Titillium+Web:wght@400;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
